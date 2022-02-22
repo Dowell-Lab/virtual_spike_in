91a06085801919a1257d7126dc7d2990dcae01d0
Zach Maas <zach.maas@gmail.com>
2022-02-22 14:13:38 -0700

    Updates to logging and serialization before refactoring into CLI

--- src/human/virtual_spike_in.py
+++ src/human/virtual_spike_in.py
@@ -38,14 +38,32 @@
 
 import itertools as it
 import re
+import pickle
+from os import path
+import time
+import logging
 import scipy.stats as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pymc3 as pm
-import pickle
-from os import path
-import time
+from tqdm import tqdm
+
+# Logger setup, only do once
+logSetup = False
+if not logSetup:
+    # Set up our module logger
+    logger = logging.getLogger("VSILogger")
+    logger.setLevel(logging.INFO)
+    fh = logging.StreamHandler()
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    # Disable most messages for pymc3 to not clutter logs
+    # pm_logger = logging.getLogger("pymc3")
+    # pm_logger.setLevel(logging.ERROR)
+    # logSetup = True
 
 # Constants required for now, eventually these will be parameterized
 baseDir = "/home/zach/Dropbox/phd/research/dna_lab/virtual_spike_in"
@@ -82,6 +100,7 @@ def import_count_data(data_csv_filename):
     )
     data["total"] = np.sum(data[7:], axis=1)
     data = data[data["total"] > 0]
+    logger.info(f"Loaded {data_csv_filename}")
     return data
 
 
@@ -89,63 +108,61 @@ def import_count_data(data_csv_filename):
 seqdata_human = import_count_data(count_data_human)
 seqdata_drosophila = import_count_data(count_data_drosophila)
 seqdata_merged = pd.concat([seqdata_human, seqdata_drosophila])
+logger.info(f"Merged VSI and Spike-In Data")
 
-print("Loaded")
 
 # We want every pairwise combination
 pairwise_combinations = list(it.combinations(sample_cols, 2))
 
 
-def lfc_model(obs, plot_model=False, num_samples=7500, burn=1000):
+def log2(x):
+    """Wrapper to support log2 natively in pymc3"""
+    return pm.math.log(x) / np.log(2)
+
+
+def lfc_model(obs, a, b, plot_model=False, num_samples=5_000, burn=2_500):
     """The definition for our linear log-fold change model"""
     with pm.Model() as model:
         # Calculate data for priors
         # data_var = np.var(obs)
         # data_mean = np.mean(obs)
         data_var = 1
-        data_mean = 0
+        # Each of our data sets are negative binomial modeled as read counts
+        # We use Laplace smoothing to avoid log problems
+        a_dist = pm.NegativeBinomial("a_dat", mu=np.mean(a + 1), alpha=1)
+        b_dist = pm.NegativeBinomial("b_dat", mu=np.mean(b + 1), alpha=1)
         # We expect variance to be positive, so use a Exponential. Half
         # cauchy also makes sure we won't hit invalid negative values
         std = pm.Exponential("std", lam=1 / data_var)
         # Normal or Cauchy works here, but the steepness of Cauchy
         # seems to make the model converge more effectively
-        mean = pm.Normal("mean", mu=data_mean, sigma=data_var, testval=0.1)
+        mean = pm.Normal(
+            "mean",
+            mu=log2((a_dist) / (b_dist)),
+            sigma=data_var,
+            testval=1,
+        )
         # Fold changes are log-normally distributed. By using the log
         # of the ratio, we transform it to be normally distributed,
         # making parameter inference simpler.
-        obs = pm.Normal("obs", mu=mean, sigma=std, observed=obs, testval=0.1)
+        obs = pm.Normal("obs", mu=mean, sigma=std, observed=obs, testval=1)
 
         # Plot the model if we explicitly request it
         if plot_model:
             pm.model_to_graphviz(model).render("model_graph.gv")
 
-        mcmc = True
-        # Run the actual sampling process in MCMC
-        if mcmc:
-            trace = pm.sample(
-                draws=num_samples,
-                tune=burn,
-                init="auto",
-            )
-        # Otherwise run variational inference using normalizing flows
-        else:
-            n, r = 10000, 0.1
-            tracker = pm.callbacks.Tracker()
-            trace_empirical = pm.fit(
-                int(1e5),
-                method="nfvi",
-                inf_kwargs=dict(n_particles=n),
-                obj_optimizer=pm.adamax(learning_rate=r),
-                progressbar=True,
-                callbacks=[
-                    pm.callbacks.CheckParametersConvergence(
-                        every=100, tolerance=1e-4, diff="relative"
-                    ),
-                    tracker,
-                ],
-            )
-            trace = trace_empirical.sample(draws=5000)
-        return trace
+        # Run the actual sampling process in MCMC. The discrete
+        # elements of the model preclude the use of variational
+        # inference.
+        trace = pm.sample(
+            draws=num_samples,
+            tune=burn,
+            init="auto",
+            return_inferencedata=False,
+        )
+        # Remove samples used for initialization
+        trace_burned = trace[burn:]
+        return trace_burned
 
 
 def run_normalization_model(i, j, tag, data):
@@ -156,23 +173,35 @@ def run_normalization_model(i, j, tag, data):
 
     # Each run gets a unique design string
     design = f"{i}_vs_{j}"
+    logger.info(f"Running {design}")
 
     # We express fold-changes in the log transformed form, since
     # log-transformed fold-change values are normally distributed.
+    data_a = data[i]
+    data_b = data[j]
+    data_ratio = np.divide(data_a, data_b)
+    np.seterr(divide="ignore")
     data_ratio = np.log2(
-        np.divide(
-            data[i],
-            data[j],
-        )
+        data_ratio,
     )
-    # data_ratio = data_ratio[~np.isnan(data_ratio)]
-    # data_ratio = data_ratio[np.isfinite(data_ratio)]
-    data_ratio[np.isnan(data_ratio)] = 0
-    data_ratio[~np.isfinite(data_ratio)] = 0
+    np.seterr(divide="warn")
+    # Two different ways to handle this -- dropping zeros or coercing
+    # them to ones. Still need to handle small sample sizes...
+    laplace_smoothing = True
+    if laplace_smoothing:
+        logger.info(f"Using Laplace Smoothing on {design}")
+        data_ratio[np.isnan(data_ratio)] = 0
+        data_ratio[~np.isfinite(data_ratio)] = 0
+    else:
+        logger.info(f"Dropping zero values on {design}")
+        data_ratio = data_ratio[~np.isnan(data_ratio)]
+        data_ratio = data_ratio[np.isfinite(data_ratio)]
+        data_ratio = data_ratio[data_ratio != 0]
+    logger.info(f"Number of samples: {len(data_ratio)}")
     ratio = np.log2(spike_in_dict[i] / spike_in_dict[j])
 
     # Execute the model
-    trace = lfc_model(data_ratio)
+    trace = lfc_model(data_ratio, data_a, data_b)
 
     # Gather results
     mu = np.mean(trace["mean"])
@@ -180,7 +209,7 @@ def run_normalization_model(i, j, tag, data):
 
     # Finish timing
     end = time.time()
-    print(f"{design} took: {end-start:.2f}s")
+    logger.info(f"{design} took: {end-start:.2f}s")
 
     # Output results
     return dict(design=design, mu=mu, sigma=sigma, ratio=ratio)
@@ -190,40 +219,31 @@ def run_normalization_model(i, j, tag, data):
 # be formalized and cleaned up
 run_model = True
 last_run_path = f"{baseDir}/dat/last_run.pickle"
-iter_dat = None
+iter_dat = []
 if run_model or not path.exists(last_run_path):
-    print("Performing Inference on Human Data")
-    results_human = [
-        run_normalization_model(i, j, "human", seqdata_human)
-        for (i, j) in pairwise_combinations
-    ]
-    print("Performing Inference on Drosophila Data")
-    results_drosophila = [
-        run_normalization_model(i, j, "drosophila", seqdata_drosophila)
-        for (i, j) in pairwise_combinations
-    ]
-    print("Performing Inference on Merged Data")
-    results_merged = [
-        run_normalization_model(i, j, "merged", seqdata_merged)
-        for (i, j) in pairwise_combinations
-    ]
-    # Merge data
-    iter_dat = list(
-        map(list, zip(*[results_human, results_drosophila, results_merged]))
-    )
+    logger.info("Performing Inference on All Models")
+    for (i, j) in tqdm(pairwise_combinations, desc="Model Fits", unit="model"):
+        result_human = run_normalization_model(i, j, "human", seqdata_human)
+        result_drosophila = run_normalization_model(
+            i, j, "drosophila", seqdata_drosophila
+        )
+        result_merged = run_normalization_model(i, j, "merged", seqdata_merged)
+        iter_dat.append((result_human, result_drosophila, result_merged))
     # Write result
     with open(last_run_path, "wb") as last_run:
         pickle.dump(iter_dat, last_run)
+        logger.info(f"Pickled results to {last_run_path}")
 else:
-    with open(last_run_path, "wb") as last_run:
-        iter_dat = pickle.read(last_run)
+    with open(last_run_path, "rb") as last_run:
+        iter_dat = pickle.load(last_run)
+        logger.info(f"Loaded results to {last_run_path}")
 
 
 # Do output printing and quantitation
-print("Plotting Model Output")
+logger.info("Plotting Model Output")
 
 
-def plot_model_output(ax, result_human, result_drosophila, result_merge, n=100):
+def plot_model_output(ax, result_human, result_drosophila, result_merge, n=250):
     # Grab Data
     plot_title = result_human["design"]
     mu_human = result_human["mu"]
@@ -240,6 +260,8 @@ def plot_model_output(ax, result_human, result_drosophila, result_merge, n=100):
     y_merge = st.norm.pdf(x, loc=mu_merge, scale=sigma_merge)
 
     # Compute statistics
+    # TODO What is this probability?
+    # A normal statistic doesn't make sens b/c no sample size...
     norm_human = np.exp(mu_human)
     norm_drosophila = np.exp(mu_drosophila)
     mean_norm = (norm_human + norm_drosophila) / 2
@@ -247,19 +269,21 @@ def plot_model_output(ax, result_human, result_drosophila, result_merge, n=100):
     pdiff = mean_norm * dnorm
 
     # Perform plotting
-    ax.plot(x, y_human, color="blue")
-    ax.plot(x, y_drosophila, color="green")
-    ax.plot(x, y_merge, color="orange")
+    ax.plot(x, y_human, color="blue", label="human")
+    ax.plot(x, y_drosophila, color="green", label="drosophila")
+    ax.plot(x, y_merge, color="orange", label="merged")
     ax.axvline(0, color="grey", linewidth=0.25)
     ax.annotate(f"fc = {pdiff:.3f}", xy=(0.1, 0.8), xycoords="axes fraction")
     ax.set_title(plot_title)
     ax.set_xlabel("Log_2(Normalization Factor)")
     ax.set_ylabel("Relative Proportion")
+    ax.legend()
+    logger.info(f"Plotted {plot_title}")
 
 
 # Plot result naively
 ncol = 4
-nrow = int(np.ceil(len(results_human) / ncol))
+nrow = int(np.ceil(len(iter_dat) / ncol))
 fig, axs = plt.subplots(
     nrow, ncol, constrained_layout=True, figsize=(18.0, 36.0)
 )
